5ed486bb4e249270106bd0817f8623487a1a8d6e
Stephanie Hamilton <hamiltons621@gmail.com>
2022-03-05 14:32:53 -0600

    fix: login user

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -43,10 +43,10 @@ export const Login = () => {
             </dialog>
             <section>
                 <form className="form--login" onSubmit={handleLogin}>
-                    <h1>Level Up</h1>
+                    <h1>Project: Pantry</h1>
                     <h2>Please sign in</h2>
                     <fieldset>
-                        <label htmlFor="inputUsername"> Username address </label>
+                        <label htmlFor="inputUsername"> Username </label>
                         <input ref={username} type="username" id="username" className="form-control" placeholder="Username address" required autoFocus />
                     </fieldset>
                     <fieldset>
