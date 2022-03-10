4aebae281a5fa67fead72c57610b295ec7a36152
Stephanie Hamilton <hamiltons621@gmail.com>
2022-03-09 22:03:44 -0600

    fix: updated ApplicationViews.js

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,13 +1,20 @@
 import React from "react"
 import { Route } from "react-router-dom"
 
+import { FoodProvider } from "./food/FoodProvider.js"
+import { FoodList } from "./food/FoodList.js"
+
 export const ApplicationViews = () => {
-    return <>
-        <main style={{
-            margin: "5rem 2rem",
-            backgroundColor: "lightgoldenrodyellow"
-        }}>
-            Application views
-        </main>
-    </>
+  return <>
+    <main style={{
+      margin: "5rem 2rem"
+    }}>
+      <h1>Application views</h1>
+      <FoodProvider>
+        <Route exact path="/foods">
+          <FoodList />
+        </Route>
+      </FoodProvider>
+    </main>
+  </>
 }
