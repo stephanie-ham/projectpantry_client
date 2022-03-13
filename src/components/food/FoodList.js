bfc5d5982d492db414fcd4a902c5b600ed5cb75b
Stephanie Hamilton <hamiltons621@gmail.com>
2022-03-12 19:42:51 -0600

    FEAT: food list

--- src/components/food/FoodList.js
+++ src/components/food/FoodList.js
@@ -2,8 +2,9 @@ import React, { useContext, useEffect } from "react";
 import { FoodContext } from "./FoodProvider";
 import { Header } from "../header/header";
 import Table from "react-bootstrap/Table";
-
+import Form from "react-bootstrap/Form";
 import "./food.css"
+import "../../styles/table.css"
 
 
 export const FoodList = (props) => {
@@ -15,30 +16,57 @@ export const FoodList = (props) => {
 
   return (
     <>
-      <Header 
+      <Header
         header={`Stocked Foods`}
       />
-      <section className="body__container">
+      <section className="body__container center">
         <Table className="food__table">
           <thead>
             <tr>
+              <th className="table__cell">Safe Food</th>
               <th className="table__cell">Name</th>
               <th className="table__cell">Location</th>
+              <th className="table__cell">Tags</th>
               <th className="table__cell">Quantity</th>
+              <th className="table__cell">Buttons</th>
             </tr>
           </thead>
           <tbody>
             {foods.map((food) => {
               return (
-                <tr className="table__row">
+                <tr className="table__row" key={`food--${food.id}`}>
+                  <td className="table__cell safe-food">
+                    <Form>
+                      <Form.Switch
+                        type="switch"
+                        id="custom-switch"
+                      />
+                    </Form>
+                  </td>
                   <td className="table__cell name">
                     {food.name}
                   </td>
                   <td className="table__cell location">
                     {food.location.title}
                   </td>
+                  <td className="table__cell tags">
+                    {food.tags.map((tag) => {
+                      return (
+                        <div
+                          className="food__tag tag"
+                          key={`food-${food.id}_tag-${tag.id}`}
+                        >
+                          {tag.label}
+                        </div>
+                      )
+                    })}
+                  </td>
                   <td className="table__cell quantity">
-                    {food.quantity.title}
+                    {/* {food.quantity.title} */}
+                    high low out add_SL
+                  </td>
+                  <td className="table__cell food-buttons">
+                    edit del
                   </td>
                 </tr>
               )
