31bc5ff37dc1ffbddb0a1db896463e2b148a9478
Stephanie Hamilton <hamiltons621@gmail.com>
2022-03-20 01:15:46 -0500

    fix: routing

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -6,24 +6,28 @@ import { FoodList } from "./food/FoodList.js"
 import { TagProvider } from "./tag/TagProvider.js"
 import { TagList } from "./tag/TagList.js"
 
+
 export const ApplicationViews = () => {
   return <>
     <main id="bootstrap-overrides">
       <TagProvider>
-      <FoodProvider>
-        <Route exact path="/foods">
-          <FoodList />
-        </Route>
-        <Route exact path="/foods/new">
-          <FoodList form={true} />
-        </Route>
-        <Route exact path="/tags">
-          <TagList />
-        </Route>
-        <Route exact path="/tags/new">
-          <TagList form={true} />
-        </Route>
-      </FoodProvider>
+        <FoodProvider>
+          <Route exact path="/foods">
+            <FoodList />
+          </Route>
+          <Route exact path="/foods/new">
+            <FoodList form={true} />
+          </Route>
+          <Route exact path="/tags">
+            <TagList />
+          </Route>
+          <Route exact path="/tags/new">
+            <TagList form={true} />
+          </Route>
+          <Route exact path="/tags/:tagId(\d+)/edit">
+            <TagList form={true} />
+          </Route>
+        </FoodProvider>
       </TagProvider>
     </main>
   </>
